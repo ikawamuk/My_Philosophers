02bac82226ba441997ac5fe6a11641c4d6311ca0
ikawamuk <ikawamuk@student.42.fr>
2025-07-23 08:25:42 +0900

    update:

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 07:14:36 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/23 08:04:16 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/23 08:25:21 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,10 @@ int	main(int ac, char *av[])
 
 	if (perse_arg(ac, av, &args) == -1)
 		return (EXIT_FAILURE);
-
+	if (philo(args) == -1)
+	{
+		printf("Error\n");
+		return (EXIT_FAILURE);
+	}
 	return (0);
 }
