47ac2d3a0000dabf4b7801f9457bd2b86618bda8
ikawamuk <ikawamuk@student.42.fr>
2025-07-23 09:54:09 +0900

    update:

---
+++ philo/src/create_threads.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_threads.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 09:53:25 by ikawamuk          #+#    #+#             */
+/*   Updated: 2025/07/23 09:53:26 by ikawamuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.h"
+
+void	*philo_loop(void *arg);
+
+int	create_threads(t_philo *philo)
+{
+	uint64_t	i;
+
+	i = 0;
+	
+	while (i < philo->ctx->philo_num)
+	{
+		pthread_create(&philo[i].thread, NULL, philo_loop, (void *)&philo[i]);
+		i++;
+	}
+	return (0);
+}
--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 08:25:50 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/23 09:48:17 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/23 09:50:40 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,13 @@ int	philo(t_args args)
 	// mallocしたもの: ctx->forks, philo, philo->finish, philo->finish_mutex
 	// mutex_initしたもの: philo->finish_mutex, 各 &philo->meal_time_mutex
 	// 　　　　　　　　　　  各philo->left_fork
+	if (create_threads(philo) == -1)
+	{
+		// free_all();
+		// mutex_destroy_all();
+		return (-1);
+	}
+	// pthread_createしたもの 各 philo->thread
 	
 	return (0);
 }
