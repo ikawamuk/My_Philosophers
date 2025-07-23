03e3ca83b6a91596b10437860a81754931c4ebed
ikawamuk <ikawamuk@student.42.fr>
2025-07-23 13:51:51 +0900

    update:

--- philo/src/eating.c
+++ philo/src/eating.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 12:27:23 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/23 13:19:25 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/23 13:33:49 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int	eating(t_philo *philo)
 	pthread_mutex_lock(&philo->meal_mutex);
 	philo->last_meal_time = get_timestamp(philo->ctx->start);
 	start_eat = philo->last_meal_time;
+	philo->eat_cnt++;
 	pthread_mutex_unlock(&philo->meal_mutex);
 	print_state(philo, philo->id, "is eating");
 	now = start_eat;
