c77c73bf8634f036ce311c0819cb2148e93b47fa
ikawamuk <ikawamuk@student.42.fr>
2025-07-26 12:40:30 +0900

    update:

--- philo_bonus/include/utils_bonus.h
+++ philo_bonus/include/utils_bonus.h
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*   utils_bonus.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/26 12:35:49 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/26 12:39:18 by ikawamuk         ###   ########.fr       */
+/*   Created: 2025/07/26 12:40:08 by ikawamuk          #+#    #+#             */
+/*   Updated: 2025/07/26 12:40:27 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo_bonus.h"
-#include <stdio.h>
+#ifndef UTILS_BONUS_H
+# define UTILS_BONUS_H
 
-int	perse_arg(int ac, char *av[], t_args *args);
 
-int	main(int ac, char *av[])
-{
-	t_args	args;
 
-	if (perse_arg(ac, av, &args) == -1)
-		return (EXIT_FAILURE);
-	if (philo_bonus(args) == -1)
-	{
-		printf("Error\n");
-		return (EXIT_FAILURE);
-	}
-	return (0);
-}
+#endif
