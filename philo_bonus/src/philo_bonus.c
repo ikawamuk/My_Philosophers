bb7234785c134b86ab6fb2fa49ef61558e99f018
ikawamuk <ikawamuk@student.42.fr>
2025-07-27 21:52:59 +0900

    update:

--- philo_bonus/src/philo_bonus.c
+++ philo_bonus/src/philo_bonus.c
@@ -6,22 +6,29 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 16:14:08 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/27 21:24:55 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/27 21:50:09 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils_bonus.h"
 
+t_philo	set_philo(uint64_t id, sem_t *running, sem_t *forks, sem_t *print);
 void	philo_process(t_philo *philo, t_args args);
 
 int	philo_bonus(t_args args)
 {
 	sem_t		*running_sem;
+	sem_t		*forks_sem;
+	sem_t		*print_sem;
 	uint64_t	i;
 	t_philo		*philo;
 
 	sem_unlink(SEM_RUNNING);
 	running_sem = sem_open(SEM_RUNNING, O_CREAT, 0644, 0);  // set_sem()
+	sem_unlink(SEM_FORKS);
+	forks_sem = sem_open(SEM_RUNNING, O_CREAT, 0644, 0);
+	sem_unlink(SEM_PRINT);
+	print_sem = sem_open(SEM_RUNNING, O_CREAT, 0644, 0); 
 	
 	philo = ft_calloc(args.philo_num, sizeof(t_philo));
 	if (!philo)
@@ -29,7 +36,7 @@ int	philo_bonus(t_args args)
 	i = 0;
 	while (i++ < args.philo_num)
 	{
-		set_philo(&philo[i]);
+		philo[i] = set_philo(i + 1, running_sem, forks_sem, print_sem);
 		philo[i].pid = fork();
 		if (philo[i].pid == 0)	
 			philo_process(&philo[i], args);
--- philo_bonus/src/set_philo_bonus.c
+++ philo_bonus/src/set_philo_bonus.c
@@ -1,20 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   set_philo.c                                        :+:      :+:    :+:   */
+/*   set_philo_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 21:03:15 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/27 21:03:58 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/27 21:52:22 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils_bonus.h"
 
-t_philo	set_philo(uint64_t id, sem_t *running_sem, t_args args)
+t_philo	set_philo(uint64_t id, sem_t *running, sem_t *forks, sem_t *print)
 {
 	t_philo	philo;
 
+	philo.id = id;
+	philo.pid = -1;
+	philo.running = running;
+	philo.forks = forks;
+	philo.print = print;
+	philo.start = 0;
+	philo.last_meal_time = 0;
+	philo.eat_cnt = 0;
 	return (philo);
 }
