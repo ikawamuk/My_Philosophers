fcd39be9fb6b4bf1846741456daf0a97a37470a4
ikawamuk <ikawamuk@student.42.fr>
2025-07-29 02:42:37 +0900

    update:

---
+++ philo_bonus/include/utils_bonus.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/26 12:40:08 by ikawamuk          #+#    #+#             */
+/*   Updated: 2025/07/29 02:42:32 by ikawamuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_BONUS_H
+# define UTILS_BONUS_H
+
+#include "config_bonus.h"
+
+int			digits_to_uint(char *str, uint64_t *rev);
+void		*ft_calloc(size_t nmemb, size_t size);
+uint64_t	get_ms(void);
+uint64_t	get_timestamp(uint64_t start);
+
+#endif
