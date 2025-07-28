1eb2082538891d2839ecad1e5131157e634ed468
ikawamuk <ikawamuk@student.42.fr>
2025-07-29 02:41:30 +0900

    update:

--- philo_bonus/include/config_bonus.h
+++ philo_bonus/include/config_bonus.h
@@ -6,13 +6,23 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 12:37:52 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/29 02:40:40 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/29 02:41:28 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONFIG_BONUS_H
 # define CONFIG_BONUS_H
 
+# include <stdlib.h>
+# include <stdint.h>
 
+typedef struct s_args
+{
+	uint64_t	philo_num;
+	uint64_t	life_time;
+	uint64_t	eat_time;
+	uint64_t	sleep_time;
+	uint64_t	must_eat;
+}	t_args;
 
 #endif
