57b96a98c2eec9dae43c601102e710aae0b014e5
ikawamuk <ikawamuk@student.42.fr>
2025-07-29 13:20:09 +0900

    update:

--- philo/src/monitor_loop.c
+++ philo/src/monitor_loop.c
@@ -6,15 +6,15 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 10:26:44 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/24 03:07:15 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/29 13:20:04 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
 static int	check_running(t_philo *philo);
-static int	someone_died(t_philo *philo);
-static int	all_philo_full(t_philo *philo, uint64_t *philo_full);
+static bool	someone_died(t_philo *philo);
+static bool	all_philo_full(t_philo *philo, uint64_t *philo_full);
 
 void	monitor_loop(t_philo *philo)
 {
@@ -41,17 +41,18 @@ static int	check_running(t_philo *philo)
 	philo_full = 0;
 	while (i < philo->ctx->philo_num)
 	{
-		if (someone_died(&philo[i]) == 1)
+		if (someone_died(&philo[i]))
 			return (0);
-		if (all_philo_full(&philo[i], &philo_full) == 1)
+		if (all_philo_full(&philo[i], &philo_full))
 			return (0);
 		i++;
 	}
 	return (1);
 }
 
-static int	someone_died(t_philo *philo)
+static bool	someone_died(t_philo *philo)
 {
+	int			died;
 	uint64_t	now;
 
 	pthread_mutex_lock(&philo->meal_mutex);
@@ -62,19 +63,17 @@ static int	someone_died(t_philo *philo)
 	{
 		philo->cmn->running = false;
 		printf("%lu\t%lu %s\n", now, philo->id, "is died");
-		pthread_mutex_unlock(&philo->cmn->print_mutex);
-		pthread_mutex_unlock(&philo->cmn->running_mutex);
-		pthread_mutex_unlock(&philo->meal_mutex);
-		return (1);
 	}
+	died = !philo->cmn->running;
 	pthread_mutex_unlock(&philo->cmn->print_mutex);
 	pthread_mutex_unlock(&philo->cmn->running_mutex);
 	pthread_mutex_unlock(&philo->meal_mutex);
-	return (0);
+	return (died);
 }
 
-static int	all_philo_full(t_philo *philo, uint64_t *philo_full)
+static bool	all_philo_full(t_philo *philo, uint64_t *philo_full)
 {
+	bool		is_all_full;
 	uint64_t	now;
 
 	if (philo->ctx->must_eat == 0)
@@ -89,13 +88,10 @@ static int	all_philo_full(t_philo *philo, uint64_t *philo_full)
 		now = get_timestamp(philo->ctx->start);
 		philo->cmn->running = false;
 		printf("%lu\tAll philo are full!\n", now);
-		pthread_mutex_unlock(&philo->cmn->print_mutex);
-		pthread_mutex_unlock(&philo->cmn->running_mutex);
-		pthread_mutex_unlock(&philo->meal_mutex);
-		return (1);
 	}
+	is_all_full = !philo->cmn->running;
 	pthread_mutex_unlock(&philo->cmn->print_mutex);
 	pthread_mutex_unlock(&philo->cmn->running_mutex);
 	pthread_mutex_unlock(&philo->meal_mutex);
-	return (0);
+	return (is_all_full);
 }
--- philo_bonus/src/monitor_loop_bonus.c
+++ philo_bonus/src/monitor_loop_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 12:22:04 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/29 12:37:08 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/29 13:17:08 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,12 @@ void	*monitor_loop(void *args)
 			break ;
 		usleep(500);
 	}
-	return (NULL);	
 	return (NULL);
 }
 
-static bool	is_died(t_philo *philo)
+static bool	died(t_philo *philo)
 {
-	bool		is_died;
+	bool		died;
 	u_int64_t	now;
 
 	sem_wait(philo->meal);
@@ -39,10 +38,27 @@ static bool	is_died(t_philo *philo)
 	sem_wait(philo->print);
 	now = get_timestamp(philo->start);
 	if (now - philo->last_meal_time > philo->args.life_time)
+	{
 		philo->someone_died = true;
-	is_died = philo->someone_died;
+		printf("%lu\t%lu %s\n", now, philo->id, "is died");
+	}
+	died = philo->someone_died;
 	sem_post(philo->print);
 	sem_post(philo->running);
 	sem_post(philo->meal);
-	return (is_died);
+	return (died);
+}
+
+static bool	is_all_full(t_philo *philo)
+{
+	bool	is_all_full;
+
+	sem_wait(philo->meal);
+	sem_wait(philo->running);
+	sem_wait(philo->print);
+	
+	sem_post(philo->print);
+	sem_post(philo->running);
+	sem_post(philo->meal);
+	return (is_all_full);
 }
