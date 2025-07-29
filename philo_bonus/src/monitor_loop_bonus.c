0911d3513f71e6128ef3618928203dde6e70c96e
ikawamuk <ikawamuk@student.42.fr>
2025-07-29 12:37:12 +0900

    update:

--- philo_bonus/src/monitor_loop_bonus.c
+++ philo_bonus/src/monitor_loop_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 12:22:04 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/29 12:32:00 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/29 12:37:08 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +31,16 @@ void	*monitor_loop(void *args)
 
 static bool	is_died(t_philo *philo)
 {
-
 	bool		is_died;
 	u_int64_t	now;
 
-	is_died = false;
 	sem_wait(philo->meal);
 	sem_wait(philo->running);
 	sem_wait(philo->print);
 	now = get_timestamp(philo->start);
 	if (now - philo->last_meal_time > philo->args.life_time)
-		philo->is_running = false;
-	is_died = true;
+		philo->someone_died = true;
+	is_died = philo->someone_died;
 	sem_post(philo->print);
 	sem_post(philo->running);
 	sem_post(philo->meal);
