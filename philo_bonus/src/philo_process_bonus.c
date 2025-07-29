259a2c7fff8a9e550af403283821655997b75bfa
ikawamuk <ikawamuk@student.42.fr>
2025-07-29 12:20:04 +0900

    update:

--- philo_bonus/src/philo_process_bonus.c
+++ philo_bonus/src/philo_process_bonus.c
@@ -6,14 +6,33 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 12:13:49 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/29 12:14:21 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/29 12:20:02 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils_bonus.h"
 
+void	*monitor_loop(void *args);
+
 void	philo_process(t_philo *philo)
 {
-	
+	sem_unlink(SEM_MEAL);
+	philo->meal = sem_open(SEM_MEAL, O_CREAT, 600, 1);
+	pthread_create(philo->thread, NULL, monitor_loop, (void *)philo);
+	sem_wait(philo->running);
+	philo->start = get_ms();
+	if (philo->id % 2 == 0)
+		usleep(1000 * philo->args.eat_time / 3);
+	while (1)
+	{
+		if (thinking(philo) == 1)
+			break ;
+		if (eating(philo) == 1)
+			break ;
+		if (sleeping(philo) == 1)
+			break ;
+	}
+	pthread_join(philo->thread, NULL);
+	exit(0);
 	return ;
 }
