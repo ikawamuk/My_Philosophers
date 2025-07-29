e8c7919bd9587dc255675fc2d69957f0e4905448
ikawamuk <ikawamuk@student.42.fr>
2025-07-29 12:23:15 +0900

    update:

--- philo_bonus/src/monitor_loop.c
+++ philo_bonus/src/monitor_loop.c
@@ -6,9 +6,17 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 12:22:04 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/29 12:22:12 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/29 12:23:13 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils_bonus.h"
 
+void	*monitor_loop(void *args)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)args;
+	
+	return (NULL);
+}
