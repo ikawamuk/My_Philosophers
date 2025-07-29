f746d2fa8120cba646694b4f2d87d1b227e0f50d
ikawamuk <ikawamuk@student.42.fr>
2025-07-29 20:52:01 +0900

    update:

--- philo_bonus/src/monitor_loop_bonus.c
+++ philo_bonus/src/monitor_loop_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 12:22:04 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/29 20:35:39 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/29 20:51:58 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,6 @@ void	*monitor_loop(void *args)
 		usleep(500);
 		if (is_dead(philo))
 			break ;
-		if (is_full(philo))
-			break ;
 	}
 	return (NULL);
 }
@@ -36,18 +34,9 @@ static bool	is_dead(t_philo *philo)
 	u_int64_t	now;
 
 	sem_wait(philo->meal);
-	
+	now = get_timestamp(philo->start);
+	if (now - philo->start > philo->args.life_time)
+		kill(0, SIGKILL);
 	sem_post(philo->meal);
 	return (died);
-}
-
-static bool	is_full(t_philo *philo)
-{
-	if (philo->args.must_eat == 0)
-		return (false);
-	sem_wait(philo->meal);
-	if (philo->eat_cnt == philo->args.must_eat)
-		sem_post(philo->running);
-	sem_post(philo->meal);
-	return (0);
 }
