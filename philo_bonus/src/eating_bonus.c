0b8424fc691d87779beac4da4631f34691ca1be3
ikawamuk <ikawamuk@student.42.fr>
2025-07-29 14:25:34 +0900

    update:

--- philo_bonus/src/eating_bonus.c
+++ philo_bonus/src/eating_bonus.c
@@ -6,14 +6,42 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 12:29:26 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/29 13:27:55 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/29 14:21:25 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils_bonus.h"
 
+static void	update_philo_state(t_philo *philo, uint64_t *start, uint64_t *now);
+
 int	eating(t_philo *philo)
 {
-	(void)philo;
+	uint64_t	start_eat;
+	uint64_t	now;
+
+	update_philo_state(philo, &start_eat, &now);
+	while (now - start_eat < philo->args.eat_time)
+	{
+		if (is_finished(philo))
+		{
+			sem_post(philo->forks);
+			sem_post(philo->forks);
+			return (1);
+		}
+		usleep(100);
+		now = get_timestamp(philo->start);
+	}
+	sem_post(philo->forks);
+	sem_post(philo->forks);
 	return (0);
+}
+
+static void	update_philo_state(t_philo *philo, uint64_t *start, uint64_t *now)
+{
+	sem_wait(philo->meal);
+	philo->last_meal_time = print_state(philo, "is eating");
+	philo->eat_cnt++;
+	*start = philo->last_meal_time;
+	*now = *start;
+	sem_post(philo->meal);
 }
--- philo_bonus/src/thinking_bonus.c
+++ philo_bonus/src/thinking_bonus.c
@@ -6,14 +6,39 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 12:28:51 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/29 13:27:41 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/29 14:24:06 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils_bonus.h"
 
+static int	take_fork(t_philo *philo);
+
 int	thinking(t_philo *philo)
 {
-	(void)philo;
+	print_state(philo, "is thinking");
+	usleep(200);
+	if (take_fork(philo) == -1)
+		return (1);
+	if (take_fork(philo) == -1)
+	{
+		sem_post(philo->forks);
+		return (1);
+	}
+	return (0);
+}
+
+static int	take_fork(t_philo *philo)
+{
+	if (is_finished(philo))
+		return (-1);
+	if (philo->args.philo_num != 1)
+		sem_wait(philo->forks);
+	print_state(philo, "has taken a fork");
+	if (philo->args.philo_num == 1)
+	{
+		usleep(1000 * philo->args.life_time);
+		return (-1);
+	}
 	return (0);
 }
