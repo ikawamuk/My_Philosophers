65dc1700d677fc5c792b03dc8e3fa9e4014e0c24
ikawamuk <ikawamuk@student.42.fr>
2025-07-29 13:26:04 +0900

    update:

--- philo_bonus/src/monitor_loop_bonus.c
+++ philo_bonus/src/monitor_loop_bonus.c
@@ -6,11 +6,14 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 12:22:04 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/29 13:17:08 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/29 13:25:59 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils_bonus.h"
+
+static bool	someone_died(t_philo *philo);
+static bool	is_all_full(t_philo *philo);
 
 void	*monitor_loop(void *args)
 {
@@ -19,7 +22,7 @@ void	*monitor_loop(void *args)
 	philo = (t_philo *)args;
 	while (1)
 	{
-		if (is_died(philo))
+		if (someone_died(philo))
 			break ;
 		if (is_all_full(philo))
 			break ;
@@ -28,7 +31,7 @@ void	*monitor_loop(void *args)
 	return (NULL);
 }
 
-static bool	died(t_philo *philo)
+static bool	someone_died(t_philo *philo)
 {
 	bool		died;
 	u_int64_t	now;
@@ -51,12 +54,21 @@ static bool	died(t_philo *philo)
 
 static bool	is_all_full(t_philo *philo)
 {
-	bool	is_all_full;
+	bool		is_all_full;
+	uint64_t	now;
 
+	is_all_full = false;
 	sem_wait(philo->meal);
 	sem_wait(philo->running);
 	sem_wait(philo->print);
-	
+	if (philo->eat_cnt == philo->args.must_eat)
+		philo->full_cnt++;
+	if (philo->full_cnt == philo->args.philo_num)
+	{
+		now = get_timestamp(philo->start);
+		printf("%lu\tAll philo are full!\n", now);
+		is_all_full = true;
+	}
 	sem_post(philo->print);
 	sem_post(philo->running);
 	sem_post(philo->meal);
