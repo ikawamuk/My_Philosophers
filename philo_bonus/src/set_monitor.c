63b5d017db7bdefb5219d079b48d5d32c9a959c2
ikawamuk <ikawamuk@student.42.fr>
2025-07-31 00:54:02 +0900

    update:

--- philo_bonus/src/set_monitor.c
+++ philo_bonus/src/set_monitor.c
@@ -6,15 +6,18 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 00:33:02 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/31 00:52:39 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/31 00:53:34 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils_bonus.h"
 
+static void	monitor_full(t_philo *philo, pid_t *pids);
+static void	monitor_dead(t_philo *philo, pid_t *pids);
 static void	kill_all_philo(uint64_t philo_num, pid_t *pids);
+static void	wait_all_process(uint64_t philo_num, pid_t *pids);
 
-void	finish_simulation(t_philo *philo, pid_t *pids)
+void	set_monitor(t_philo *philo, pid_t *pids)
 {
 	pthread_create(monitor_full, NULL, monitor_full, (void *)philo);
 	pthread_create(monitor_dead, NULL, monitor_dead, (void *)philo);
