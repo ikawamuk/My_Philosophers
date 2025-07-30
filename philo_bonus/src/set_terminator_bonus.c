a3be7c6ab06ff25753a73853265f01fcd9e6280d
ikawamuk <ikawamuk@student.42.fr>
2025-07-31 02:20:23 +0900

    update:

--- philo_bonus/src/set_terminator_bonus.c
+++ philo_bonus/src/set_terminator_bonus.c
@@ -1,34 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   set_monitor_bonus.c                                :+:      :+:    :+:   */
+/*   set_terminator_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 00:33:02 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/31 02:10:41 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/31 02:19:58 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils_bonus.h"
 
-static void	*monitor_full(void *arg);
-static void *monitor_dead(void *arg);
+static void	*terminator_full(void *arg);
+static void *terminator_dead(void *arg);
 static void	kill_all_philo(uint64_t philo_num, pid_t *pids);
 
-void	set_monitor(t_philo *philo)
+void	set_terminator(t_philo *philo)
 {
 	pthread_t	full;
 	pthread_t	dead;
 
-	pthread_create(&dead, NULL, monitor_dead, (void *)philo);
-	pthread_create(&full, NULL, monitor_full, (void *)philo);
+	pthread_create(&dead, NULL, terminator_dead, (void *)philo);
+	pthread_create(&full, NULL, terminator_full, (void *)philo);
 	pthread_detach(dead);
 	pthread_detach(full);
 	return ;
 }
 
-static void	*monitor_dead(void *arg)
+static void	*terminator_dead(void *arg)
 {
 	t_philo	*philo;
 
@@ -38,7 +38,7 @@ static void	*monitor_dead(void *arg)
 	return (NULL);
 }
 
-static void	*monitor_full(void *arg)
+static void	*terminator_full(void *arg)
 {
 	t_philo	*philo;
 	uint64_t	i;
