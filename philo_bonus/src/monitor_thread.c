1ac3bc3037d060690b488cdccca67231cbf91c1e
ikawamuk <ikawamuk@student.42.fr>
2025-07-31 03:03:15 +0900

    update:

--- philo_bonus/src/monitor_thread.c
+++ philo_bonus/src/monitor_thread.c
@@ -1,34 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo_process_bonus.c                              :+:      :+:    :+:   */
+/*   monitor_thread.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/31 01:05:52 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/31 03:01:24 by ikawamuk         ###   ########.fr       */
+/*   Created: 2025/07/31 03:01:40 by ikawamuk          #+#    #+#             */
+/*   Updated: 2025/07/31 03:02:47 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils_bonus.h"
 
-void	thinking(t_philo *philo);
-void	eating(t_philo *philo);
-void	sleeping(t_philo *philo);
-
-void	*philo_process(void *arg)
+void	*monitor_thread(void *arg)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
-	create_monitor(philo);
-	sem_wait(philo->sems.start);
-	philo->start = get_ms();
-	while (1)
-	{
-		thinking(philo);
-		eating(philo);
-		sleeping(philo);
-	}
 	return (NULL);
 }
